791575cb86aedabbb86d8c304b6ca43ab0a5715c
Luiz Fernando <60102191+luizfernandofo@users.noreply.github.com>
2022-07-22 09:43:03 -0300

    First commit

---
+++ src/main/java/br/ufg/ufgmotors/UfgmotorsApplication.java
@@ -0,0 +1,13 @@
+package br.ufg.ufgmotors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfgmotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfgmotorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufg/ufgmotors/UfgmotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufg.ufgmotors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfgmotorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
