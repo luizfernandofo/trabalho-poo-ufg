d935e46975e8bb16480e85958974276756b1540b
Luiz Fernando <60102191+luizfernandofo@users.noreply.github.com>
2022-07-24 22:49:49 -0300

    Classe User e UserController

---
+++ src/main/java/model/User.java
@@ -0,0 +1,20 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    String name;
+    String email;
+    String password;
+    Boolean signedIn;
+    LocalDateTime signUpDate;
+
+}
